0d617ad9dddc68ef90b9d557326efeee81d19aa3
lanjelot <lanjelot@gmail.com>
2021-03-23 23:13:43 +1100

    Use localtime built-in filter

--- ctftools/settings.py
+++ ctftools/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ctfpad.middleware.TimezoneMiddleware'
 ]
 
 ROOT_URLCONF = 'ctftools.urls'
