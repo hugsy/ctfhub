efacc52f57d2bd6e666da71b81ad92d0aef8c631
hugsy <hugsy@blah.cat>
2023-06-22 14:02:43 -0700

    applied djjs to static/js

--- static/js/challenge.js
+++ static/js/challenge.js
@@ -1,28 +1,28 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-            var formatSelect = document.getElementById('id_format');
-            var dataTextArea = document.getElementById('id_data');
+    var formatSelect = document.getElementById('id_format');
+    var dataTextArea = document.getElementById('id_data');
 
-            formatSelect.addEventListener('change', function () {
-                var selectedFormat = this.value;
-                var placeholderText;
+    formatSelect.addEventListener('change', function () {
+        var selectedFormat = this.value;
+        var placeholderText;
 
-                switch (selectedFormat) {
-                    case 'RAW':
-                        placeholderText = 'name | category';
-                        break;
-                    case 'CTFd':
-                        placeholderText = 'paste CTFd JSON /api/v1/challenges';
-                        break;
-                    case 'rCTF':
-                        placeholderText = 'paste rCTF JSON /api/v1/challs';
-                        break;
-                    default:
-                        placeholderText = '';
-                }
+        switch (selectedFormat) {
+            case 'RAW':
+                placeholderText = 'name | category';
+                break;
+            case 'CTFd':
+                placeholderText = 'paste CTFd JSON /api/v1/challenges';
+                break;
+            case 'rCTF':
+                placeholderText = 'paste rCTF JSON /api/v1/challs';
+                break;
+            default:
+                placeholderText = '';
+        }
 
-                dataTextArea.setAttribute('placeholder', placeholderText);
-            });
+        dataTextArea.setAttribute('placeholder', placeholderText);
+    });
 
             // Trigger the change event on page load to set initial placeholder
-            formatSelect.dispatchEvent(new Event('change'));
+    formatSelect.dispatchEvent(new Event('change'));
 });
