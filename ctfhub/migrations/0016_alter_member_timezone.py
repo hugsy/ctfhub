c04a876e6e3f78fa1b3d4fb8b159268e76323549
hugsy <hugsy@blah.cat>
2023-06-28 22:38:21 -0700

    and fixed migration dependency

--- ctfhub/migrations/0016_alter_member_timezone.py
+++ ctfhub/migrations/0016_alter_member_timezone.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("ctfhub", "0016_alter_member_timezone"),
+        ("ctfhub", "0015_alter_ctf_note_id_alter_member_timezone"),
     ]
 
     operations = [
