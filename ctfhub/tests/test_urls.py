cf3c05c41dbab5e55bc91cdf638d2d7ca1c51b70
hugsy <hugsy@blah.cat>
2023-06-30 12:59:51 -0700

    typo in test_urls

--- ctfhub/tests/test_urls.py
+++ ctfhub/tests/test_urls.py
@@ -28,8 +28,6 @@ class UrlsTest(SimpleTestCase):
         url = "/teams/edit/1"
         self.assertEqual(resolve(url).func.view_class, teams.TeamUpdateView)
 
-    # Add tests for other team URLs
-
     def test_users_list_url_resolves(self):
         url = "/users/"
         self.assertEqual(resolve(url).func.view_class, users.MemberListView)
@@ -37,8 +35,6 @@ class UrlsTest(SimpleTestCase):
     def test_users_add_url_resolves(self):
         url = "/users/add/"
         self.assertEqual(resolve(url).func.view_class, users.MemberCreateView)
-
-    # Add tests for other user URLs
 
     def test_dashboard_url_resolves(self):
         url = "/dashboard/"
@@ -52,25 +48,13 @@ class UrlsTest(SimpleTestCase):
         url = "/stats/"
         self.assertEqual(resolve(url).func, generate_stats)
 
-    # Add tests for other stats URLs
-
     def test_ctfs_list_url_resolves(self):
         url = "/ctfs/"
         self.assertEqual(resolve(url).func.view_class, ctfs.CtfListView)
 
-    # Add tests for other CTF URLs
-
     def test_challenges_list_url_resolves(self):
         url = "/challenges/"
         self.assertEqual(resolve(url).func.view_class, challenges.ChallengeListView)
-
-    # Add tests for other challenge URLs
-
-    def test_files_add_url_resolves(self):
-        url = "/challenges/<uuid:challenge_id>/files/add/"
-        self.assertEqual(resolve(url).func.view_class, files.ChallengeFileCreateView)
-
-    # Add tests for other file URLs
 
     def test_categories_create_url_resolves(self):
         url = "/categories/create/"
@@ -79,7 +63,3 @@ class UrlsTest(SimpleTestCase):
     def test_tags_list_url_resolves(self):
         url = "/tags/"
         self.assertEqual(resolve(url).func.view_class, tags.TagListView)
-
-    # Add tests for other tag URLs
-
-    # Add more tests for other URLs in your `urls.py` file
