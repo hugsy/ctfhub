993318734e739c060144ec693c6c9b6c7c40a7a9
hugsy <hugsy@blah.cat>
2023-07-03 16:59:57 -0700

    using storage for Team model too

--- ctfhub_project/settings.py
+++ ctfhub_project/settings.py
@@ -131,18 +131,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+USE_TZ = False
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
